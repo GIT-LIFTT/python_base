8ea7e76c86c8f70cb39680d94dcc036c3bf74a02
git-liftt <jonathan_s7@hotmail.co.uk>
2019-12-14 02:50:55 +0000

    holly shiz

--- ping_test.py
+++ ping_test.py
@@ -1,3 +1,7 @@
 from pythonping import ping
 1
-ping('100.100.100.3')
+ping('100.100.100.3',verbose=True)
+ping('100.100.100.3',verbose=True)
+ping('100.100.100.3',verbose=True)
+ping('100.100.100.3',verbose=True)
+ping('100.100.100.3',verbose=True)
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 import paramiko
-ip_address = "100.100.100.2"
+import time
+ip_address = "100.100.100.3"
+username = "cisco"
+password = "cisco"
+
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname=ip_address,username=username,password=password)
+print ("connected to ", ip_address)
+
+time.sleep(100)
+
 print ("testing")
